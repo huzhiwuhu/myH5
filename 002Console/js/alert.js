56b6a1fcda78bd90b5ceca254de22f9e01d06eb1
huzhiwuhu <499915398@qq.com>
2019-03-22 19:29:49 +0800

    modify

---
+++ 002Console/js/alert.js
@@ -0,0 +1,14 @@
+function taolu(){
+    alert("你猜一会会发生什么");
+    alert("再等等");
+    alert("也许真有奇迹");
+}
+alert("你怎么打开了这个网页");
+taolu();
+alert("你相信奇迹吗");
+alert("让我来向你提个问题");
+taolu();
+alert("果然没有奇迹");
+alert("我们就这样放弃吗");
+taolu();
+alert("这是最后一句了，88");
